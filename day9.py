5dabb029b7a215dfc44cc7c116c028cfb6ac653f
relax-focus <54520272+relax-focus@users.noreply.github.com>
2019-10-21 10:33:02 +0800

    Add files via upload
    day9更新

--- day9.py
+++ day9.py
@@ -305,3 +305,96 @@ class Ultraman(Fighter):
 
         :return: 使用魔法成功返回True否则返回False
         """
+        if self._mp >= 20:
+            self._mp -= 20
+            for temp in others:
+                if temp.alive:
+                    temp.hp -= randint(10, 15)
+            return True
+        else:
+            return False
+
+    def resume(self):
+        """恢复魔法值"""
+        incr_point = randint(1, 10)
+        self._mp += incr_point
+        return incr_point
+
+    def __str__(self):
+        return '~~~%s奥特曼~~~\n' % self._name + \
+            '生命值：%d\n' % self._hp + \
+            '魔法值：%d\n' % self._mp
+
+class Monster(Fighter):
+    """小怪兽"""
+    __slots__ = ('_name', '_hp')
+
+    def attack(self, other):
+        other.hp -= randint(10, 20)
+
+    def __str__(self):
+        return '~~~%s小怪兽~~~\n' % self._name + \
+            '生命值：%d\n' % self._hp
+
+def is_any_alive(monsters):
+    """判断有没有小怪兽是活着的"""
+    for monster in monsters:
+        if monster.alive > 0:
+            return True
+    return False
+
+def select_alive_one(monsters):
+    """选中一只活着的小怪兽"""
+    monsters_len = len(monsters)
+    while True:
+        index = randrange(monsters_len)
+        monster = monsters[index]
+        if monster.alive > 0:
+            return monster
+
+def display_info(ultraman, monsters):
+    """显示奥特曼和小怪兽的信息"""
+    print(ultraman)
+    for monster in monsters:
+        print(monster, end='')
+
+def main():
+    u = Ultraman('骆昊', 1000, 120)
+    m1 = Monster('狄仁杰', 250)
+    m2 = Monster('白元芳', 500)
+    m3 = Monster('王大锤', 750)
+    ms = [m1, m2, m3]
+    fight_round = 1
+    while u.alive and is_any_alive(ms):
+        print('========第%02d回合========' % fight_round)
+        m = select_alive_one(ms)
+        skill = randint(1, 10)
+        if skill <= 6:
+            print('%s使用普通攻击打了%s.' % (u.name, m.name))
+            u.attack(m)
+            print('%s的魔法值恢复了%d点.' % (u.name, u.resume()))
+        elif skill <= 9:
+            if u.magic_attack(ms):
+                print('%s使用了魔法攻击.' % u.name)
+            else:
+                print('%s使用了魔法失败.' % u.name)
+        else:
+            if u.huge_attack(m):
+                print('%s使用究极必杀技虐了%s.' % (u.name, m.name))
+            else:
+                print('%s使用普通攻击打了%s.' % (u.name, m.name))
+                print('%s的魔法值恢复了%d点.' % (u.name, u.resume()))
+
+        if m.alive > 0:
+            print('%s回击了%s.' % (m.name, u.name))
+            m.attack(u)
+        display_info(u, ms)
+        fight_round += 1
+    print('\n========战斗结束!========\n')
+    if u.alive > 0:
+        print('%s奥特曼胜利!' % u.name)
+    else:
+        print('小怪兽胜利!')
+
+if __name__ == '__main__':
+    main()
